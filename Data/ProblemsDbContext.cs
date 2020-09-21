93e3ce5e8186bd49d924550a66f3e3e93c76d14d
Paulo <paulo.esantos@gmail.com>
2020-09-21 13:22:13 -0300

    Todo Items

--- Data/ProblemsDbContext.cs
+++ Data/ProblemsDbContext.cs
@@ -1,3 +1,4 @@
+using ApiProblems.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ProblemsApi.Data
@@ -5,5 +6,10 @@ namespace ProblemsApi.Data
     public class ProblemsDbContext : DbContext
     {
         public ProblemsDbContext(DbContextOptions<ProblemsDbContext> option) : base(option) { }
+    }
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<TodoItem> TodoItems { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using ProblemsApi.Data;
 
 namespace ProblemsApi
 {
@@ -25,7 +29,24 @@ namespace ProblemsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Problems API",
+                        Version = "v1",
+                        Description = "Resolução de Problemas",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Paulo Eduardo Fagundes dos Santos",
+                            Url = new Uri("https://github.com/pauloesantos")
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +56,13 @@ namespace ProblemsApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Resolução de Problemas");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
@@ -46,6 +74,7 @@ namespace ProblemsApi
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
